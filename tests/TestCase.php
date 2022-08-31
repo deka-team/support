ceecf5de3258ab72b1308726a8b040d2078cf930
riskihajar <riskihajar@users.noreply.github.com>
2022-08-31 03:41:00 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,8 @@
 
 namespace Deka\Support\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Deka\Support\SupportServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
