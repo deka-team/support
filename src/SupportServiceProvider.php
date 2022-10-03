ea9877b8d248fd267d4ce51fc8bddc36725783ca
riskihajar <riskihajar@gmail.com>
2022-10-03 12:02:26 +0800

    change ulid -> ulidAlias

--- src/SupportServiceProvider.php
+++ src/SupportServiceProvider.php
@@ -64,7 +64,7 @@ class SupportServiceProvider extends PackageServiceProvider
 
     protected function bootBlueprint()
     {
-        Blueprint::macro('ulid', function ($name = 'ulid') {
+        Blueprint::macro('ulidAlias', function ($name = 'ulid') {
             /** @var Blueprint $this */
             $this->string($name, 26)->unique();
         });
